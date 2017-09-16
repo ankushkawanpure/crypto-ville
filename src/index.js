87160324038218ac27223cd7aed01381bb79cfd7
ankushkawanpure <ankushkawanpure@gmail.com>
2017-09-16 16:29:56 -0400

    react router add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Router, Route, browserHistory} from 'react-router';
+// import Routes from './routes';
 
-import App from 'components/App';
+import './index.css';
+import App from './components/App';
+import ProducePage from './components/ProducePage';
+
+
 // import App from 'components/ProducePage';
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router
+        history={browserHistory}>
+        <Route path='/' component={App}></Route>
+        <Route path='/produce' component={ProducePage}></Route>
+        <Route path='/400' component={ProducePage}></Route>
+    </Router>,
+    document.getElementById('root')
+);
+
