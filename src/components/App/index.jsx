49ba3339b8d7e59d1cf9046743e5b5c8e85b81c3
Joe Johans <lgvichy@gmail.com>
2017-09-16 01:24:37 -0400

    Slight refactor

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import './App.css';
+
+import logo from 'assets/logo.svg';
+
+import './index.css';
 
 class App extends Component {
   render() {
