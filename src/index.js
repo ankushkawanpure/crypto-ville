3ad14f48f703606b17894b4aea120a0bcd3080c5
ankushkawanpure <ankushkawanpure@gmail.com>
2017-09-16 16:38:15 -0400

    farner route

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
-// import Routes from './routes';
 
 import './index.css';
 import App from './components/App';
 import ProducePage from './components/ProducePage';
+import FarmerPage from './components/FarmerPage';
 
 
-
-// import App from 'components/ProducePage';
 ReactDOM.render(
     <Router
         history={browserHistory}>
         <Route path='/' component={App}></Route>
         <Route path='/produce' component={ProducePage}></Route>
-        <Route path='/400' component={ProducePage}></Route>
+        <Route path='/farmer' component={FarmerPage}></Route>
     </Router>,
     document.getElementById('root')
 );
