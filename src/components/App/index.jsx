24d0fbde93e018c39abf63a3945dcf079efce02f
Joe Johans <lgvichy@gmail.com>
2017-09-16 04:36:21 -0400

    adding some random interval data

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 
 import logo from 'assets/logo.png';
 
@@ -8,30 +10,70 @@ import SearchBar from 'components/SearchBar';
 
 import './index.css';
 
-import {languages} from './variables';
+import {
+	languages
+} from './variables';
 
 const renderSuggestion = suggestion =>
-  <div>
+	<div>
     {suggestion.name}
   </div>;
 
+const sampleWatchList = ["Banana", "Apple", "Kiwi", "Durian", "Citrus", "Pear"];
+
+function getRandomArbitrary(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+
+	state = {
+		data: []
+	}
+
+	update() {
+		const newData = sampleWatchList.map((item) => {
+			return {
+				name: item,
+				price: getRandomArbitrary(1, 10)
+					.toFixed(3),
+				farmerCount: getRandomInt(10, 45)
+			}
+		})
+
+		this.setState({
+			data: newData
+		});
+	}
+
+	componentWillMount() {
+		setInterval(() => {
+			this.update();
+		}, 450);
+	}
+
+	render() {
+		return(
+			<div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>CryptoVille</h2>
 					<SearchBar dictionary={languages} renderSuggestion={renderSuggestion}/>
         </div>
 
-
-				<ProduceItem name="Banana" price={10} farmerCount={45} />
-
+				{this.state.data.map(item =>
+					<ProduceItem key={item.name} name={item.name} price={item.price} farmerCount={item.farmerCount} />
+				)}
 
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default App;
