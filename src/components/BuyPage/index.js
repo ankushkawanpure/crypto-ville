a847e8b3fa04eeaa21f619ce59e03a5ebdd8c690
Joe Johans <lgvichy@gmail.com>
2017-09-17 01:27:15 -0400

    Fix buy page

--- src/components/BuyPage/index.js
+++ src/components/BuyPage/index.js
@@ -1,14 +1,18 @@
 import React, {
-    Component
+	Component
 } from 'react';
 
-import {fetchProduceDetail, fetchProduceFarmers} from 'api';
+import {
+	fetchProduceDetail
+} from 'api';
 
 import {
-    extractMoneyValue
+	extractMoneyValue
 } from 'utils';
 
-import {currencySymbol} from 'variables';
+import {
+	currencySymbol
+} from 'variables';
 
 import NavBar from 'components/NavBar';
 
@@ -16,104 +20,109 @@ import './index.css';
 
 export default class BuyPage extends Component {
 
-    static defaultProps = {
-        currencySymbol: '$'
-    };
+	state = {
+		moneyValueObj: {},
+		produceData: {},
+		quantity: 0
+	}
 
-    state = {
-        moneyValueObj: {},
-        produceData: {},
-        items : 0
-    }
+	update() {
+		const {
+			name
+		} = this.props.match.params;
 
-    update() {
-        const {name} = this.props.match.params;
+		this.setState({
+			produceData: fetchProduceDetail(name)
+		});
+	}
 
-        this.setState({
-            produceData: fetchProduceDetail(name)
-        });
-    }
+	componentWillUnmount() {
+		clearInterval(this.updateInterval)
+	}
 
-    componentWillUnmount() {
-        clearInterval(this.updateInterval)
-    }
+	componentWillMount() {
+		this.updateInterval = setInterval(() => {
+			this.update();
+		}, 450);
+	}
 
-    componentWillMount() {
-        this.updateInterval = setInterval(() => {
-            this.update();
-        }, 450);
-    }
+	updateQuantity = (e) => {
+		const {value} = e.nativeEvent.target;
+		this.setState({
+			quantity: parseInt(value, 0) || 0
+		})
+	}
 
-    updateitems = (price) => {
+	render() {
+		const {
+			name
+		} = this.props.match.params;
 
-        this.setState({items:parseInt(price)})
-    }
+		const {
+			produceData
+		} = this.state;
 
-    render() {
-        const {name} = this.props.match.params;
-        let cost = 0;
+		const moneyValueObj = extractMoneyValue(produceData.price);
 
-        const {produceData} = this.state;
-        const moneyValueObj = extractMoneyValue(produceData.price);
-        const {valueFormated, floatingPoint} = moneyValueObj;
+		const {
+			valueFormated,
+			floatingPoint
+		} = moneyValueObj;
 
-        return(
-            <div className="BuyPage">
-                <NavBar title={`${name}`} secondaryTitle={`${name} - ${currencySymbol}${valueFormated}.${floatingPoint}`}
-                        offsetThreshold='108'
-                        showHome/>
-                <div className="Level"> </div>
+		return(
+			<div className="BuyPage">
+				<NavBar title={`${name}`} secondaryTitle={`${name} - ${currencySymbol}${valueFormated}.${floatingPoint}`}
+					offsetThreshold='108'
+					showHome/>
+				<div className="Level"> </div>
 
-                <div>
-                    <span className="BuyContainer">
+				<div>
+					<span className="BuyContainer">
 						<span className="pulltoleft">
-                            Quantity of {name}
-                        </span>
+							Quantity of {name}
+						</span>
 
 
-                        <span className="pulltoright">
-                                    <input onChange={this.updateitems} type="text" className="inverse"/>
-                        </span>
-        			</span>
-                </div>
+						<span className="pulltoright">
+							<input onChange={this.updateQuantity} type="text" className="inverse"/>
+						</span>
+					</span>
+				</div>
 
-                <div>
-                    <span className="BuyContainer">
+				<div>
+					<span className="BuyContainer">
 						<span className="pulltoleft">
-                            Marcket Price
-                        </span>
+							Marcket Price
+						</span>
 
 
-                        <span className="pulltoright">
-                                    {`${valueFormated}.${floatingPoint}`}
-                        </span>
-        			</span>
-                </div>
+						<span className="pulltoright">
+							{`${currencySymbol}${valueFormated}.${floatingPoint}`}
+						</span>
+					</span>
+				</div>
 
 
-                <div>
-                    <span className="BuyContainer">
+				<div>
+					<span className="BuyContainer">
 						<span className="pulltoleft">
-                            EST Cost
-                        </span>
+							EST Cost
+						</span>
 
 
-                        <span className="pulltoright">
-                            {parseInt(this.state.items) * parseInt(valueFormated)}
-                        </span>
-        			</span>
-                </div>
+						<span className="pulltoright">
+							{currencySymbol}{(this.state.quantity * produceData.price).toFixed(3)}
+						</span>
+					</span>
+				</div>
 
 
-                <div className="bottomContainer">
-                    <div className="BuyButton" >
-                        Order
-                    </div>
-                </div>
-
-
-            </div>
-
-        );
-    }
+				<div className="bottomContainer">
+					<div className="BuyButton" >
+						Order
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
