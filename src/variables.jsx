bb2e11fdda9711334a905ece4328bbd0d43ef7c4
Joe Johans <lgvichy@gmail.com>
2017-09-17 03:14:06 -0400

    Adding lisk

--- src/variables.jsx
+++ src/variables.jsx
@@ -1,3 +1,14 @@
+export const liskKey = "good swamp brass scissors useful useful surround sock local liquid swim lady";
+
+export const liskOption = {
+    ssl: false, // Default false. Set to true to enable the https instead of http protocol.
+    node: '', // Default randomNode. Insert a node without http or https protocol. Use ssl option in order to set http or https.
+    randomPeer: true, // Default true. Lisk-js automatically connects to a random peer to get lisk blockchain information. Set to false to disable this behaviour.
+    testnet: true, // Default false. Set to true to use the testnet. Set to false to use the mainnet.
+    port: '7000', // Default 8000. Enter the port as the protocol http(s)://node:port - can be any string.
+    bannedPeers: [], // Default empty. Array of peers that should not be connected to. Without http(s) or port.
+};
+
 export const currencySymbol = '$';
 
 export const mediaPath = '/media';
